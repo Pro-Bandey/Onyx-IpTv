7d446559198536ca6d7003fb5e97d1ecfe840bed
Pro Bandey <195909043+Pro-Bandey@users.noreply.github.com>
2026-06-18 03:27:08 +0500

    fix 4

--- scripts/logos.js
+++ scripts/logos.js
@@ -3,7 +3,7 @@ import path from 'path';
 import crypto from 'crypto';
 import sharp from 'sharp';
 
-const DB_CHANNELS_DIR = 'https://raw.githubusercontent.com/Pro-Bandey/Onyx-IpTv/db/channels';
+const DB_CHANNELS_DIR = '../db/channels';
 const LOGOS_OUT_DIR = '../logos/logos';
 const LOGOS_INDEX_FILE = '../logos/logos.json';
 
